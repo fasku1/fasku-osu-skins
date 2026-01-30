e01cfacaa412c4ede7761b876120512d1a311e40
Ethan Yang <elyang3@wisc.edu>
2026-01-29 22:23:44 -0600

    added images and url of skins currently uploaded. this can be optimized in the future

--- src/App.jsx
+++ src/App.jsx
@@ -7,6 +7,8 @@ import Card from 'react-bootstrap/Card';
 import Carousel from 'react-bootstrap/Carousel';
 import 'bootstrap/dist/css/bootstrap.min.css';
 
+
+
 function App() {
   const [count, setCount] = useState(0)
 
@@ -15,28 +17,64 @@ function App() {
       <h1>Fasku Osu Skins</h1>
       <p>this is where you can download all my osu skins :D</p>
 
-      <Card>
+      <Card className="mb-3">
         <Carousel>
           <Carousel.Item>
             <Card.Img
               variant="top"
               src="../temp/skin0-0.jpg"
-              
+              class="skin-img"
             />
           </Carousel.Item>
           <Carousel.Item>
             <Card.Img
               variant="top"
-              src="./assets/react.svg"
-              class=".skin-img-16by9"
+              src="../temp/skin0-1.jpg"
+              class="skin-img"
             />
           </Carousel.Item>
         </Carousel>
         <Card.Body>
-          <Card.Title>"Osu Skin1"</Card.Title>
+          <Card.Title>  <a
+            href="https://drive.google.com/file/d/1MEl35a3YGfV0ilUJI-RbshcN-jBJoi6l/view?usp=sharing"
+            target="_blank"
+            rel="noopener noreferrer"
+            style={{ textDecoration: 'none', color: 'inherit' }}
+          >
+            fireku v8
+          </a></Card.Title>
         </Card.Body>
       </Card>
-      
+      <Card>
+        <Carousel>
+          <Carousel.Item>
+            <div
+              className="skin-placeholder"
+              style={{
+                width: '100%',
+                height: '200px',       // same height as images
+                display: 'flex',
+                alignItems: 'center',
+                justifyContent: 'center',
+                backgroundColor: '#eee', // gray box
+                color: '#555',
+                fontSize: '1.2rem',
+              }}
+            >No img yet :(</div>
+          </Carousel.Item>
+        </Carousel>
+        <Card.Body>
+          <Card.Title>  <a
+            href="https://drive.google.com/file/d/1QRL7XPXKLdeyGbppr-y4Cug6OmMh5vUI/view?usp=sharing"
+            target="_blank"
+            rel="noopener noreferrer"
+            style={{ textDecoration: 'none', color: 'inherit' }}
+          >
+            bashua priv skin
+          </a></Card.Title>
+        </Card.Body>
+      </Card>
+
     </>
   )
 }
