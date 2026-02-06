7a06f9deafc4c6f8b8684921ba7c3666221bb628
Ethan Yang <elyang3@wisc.edu>
2026-02-05 19:37:48 -0600

    matched styling to Fasku's Social page. additionally added in sounds

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,6 @@
+import 'bootstrap/dist/css/bootstrap.min.css';
+
 import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
 import './App.css'
 import Button from 'react-bootstrap/Button';
 import Card from 'react-bootstrap/Card';
@@ -8,32 +8,38 @@ import Carousel from 'react-bootstrap/Carousel';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import skin00 from './assets/skin-images/skin0-0.jpg';
 import skin01 from './assets/skin-images/skin0-1.jpg';
-
-
+import wiiClick from './assets/sounds/wii-click.mp3'
+import wiiHover from './assets/sounds/wii-hover.wav'
 
 
 function App() {
   const [count, setCount] = useState(0)
 
+    function click() {
+    const audio = new Audio(wiiClick)
+    audio.volume = 0.5 // 20% volume
+    audio.play()
+  }
+
   return (
     <>
-      <h1>Fasku Osu Skins</h1>
-      <p>this is where you can download all my osu skins :D</p>
+      <h1 className="fw-bold light" style={{ color: '#FFFFFF' }}>Fasku Osu Skins</h1>
+      <p className="light" style={{ color: '#FFFFFF' }}>this is where you can download all my osu skins :D</p>
 
       <Card className="mb-3">
-        <Carousel>
+        <Carousel onSelect={click} interval={null}>
           <Carousel.Item>
             <Card.Img
               variant="top"
               src={skin00}
-              class="skin-img"
+              className="skin-img" // Use className in React, not class
             />
           </Carousel.Item>
           <Carousel.Item>
             <Card.Img
               variant="top"
               src={skin01}
-              class="skin-img"
+              className="skin-img"
             />
           </Carousel.Item>
         </Carousel>
@@ -42,17 +48,17 @@ function App() {
             href="https://drive.google.com/file/d/1MEl35a3YGfV0ilUJI-RbshcN-jBJoi6l/view?usp=sharing"
             target="_blank"
             rel="noopener noreferrer"
-            style={{ textDecoration: 'none', color: 'inherit' }}
+            style={{ textDecoration: 'none', color: '#A2FF00' }}
           >
             fireku v8
           </a></Card.Title>
         </Card.Body>
       </Card>
       <Card>
-        <Carousel>
+        <Carousel onSelect={click} interval={null}>
           <Carousel.Item>
             <div
-              className="skin-placeholder"
+              className="skin-placeholder card"
               style={{
                 width: '100%',
                 height: '200px',       // same height as images
@@ -62,6 +68,7 @@ function App() {
                 backgroundColor: '#eee', // gray box
                 color: '#555',
                 fontSize: '1.2rem',
+                color: '#ffffffa7'
               }}
             >No img yet :(</div>
           </Carousel.Item>
@@ -71,7 +78,7 @@ function App() {
             href="https://drive.google.com/file/d/1QRL7XPXKLdeyGbppr-y4Cug6OmMh5vUI/view?usp=sharing"
             target="_blank"
             rel="noopener noreferrer"
-            style={{ textDecoration: 'none', color: 'inherit' }}
+            style={{ textDecoration: 'none', color: '#A2FF00' }}
           >
             bashua priv skin
           </a></Card.Title>
